0b71dc0ea4d8274d3b3d1efec081ac353de2a36c
MTsuyama-git <miyabi-yamabiko@hotmail.co.jp>
2023-03-15 16:03:53 +0900

    added websocket_sample

---
+++ test/websocket_sample.cpp
@@ -0,0 +1,14 @@
+#include <TcpServer.hpp>
+#include <BindConfig.hpp>
+#include <unistd.h>   // close()
+#include <sys/stat.h> // struct stat
+#include <libgen.h>
+#include <stdexcept>
+#include <iostream>
+
+#define READ_BUFFER_LEN 2048
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
