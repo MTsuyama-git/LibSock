c8f032cae960a71f1cdbf9b0096441d2d42d6940
MTsuyama-git <miyabi-yamabiko@hotmail.co.jp>
2023-03-16 22:04:29 +0900

    update

--- test/websocket_sample.cpp
+++ test/websocket_sample.cpp
@@ -8,7 +8,18 @@
 #include <iostream>
 #include <filesystem>
 #include <fstream>
+#include <cmath>
 #include <map>
+
+extern "C"
+{
+#include <openssl/sha.h>
+#include <openssl/evp.h>
+#include <openssl/pem.h>
+#include <openssl/bio.h>
+#include <openssl/err.h>
+#include <openssl/rsa.h>
+}
 
 #define SERVER_SIGNATURE "tiny-server 1.0.0"
 #define RESP "HTTP/1.1 %d %s\r\n"
@@ -23,9 +34,14 @@ using namespace std::filesystem;
 
 void servWS(void);
 void servHTML(int servSock);
+void printBuffer(void *buffer, size_t length);
 
 char read_buffer[READ_BUFFER_LEN];
 char resp_buffer[RESP_BUFFER_LEN];
+unsigned char websocket_sha1_buf[512];
+unsigned char websocket_b64_buf[512];
+
+std::vector<int> wsClients;
 const path assets_dir = "./statics";
 
 typedef enum
@@ -34,7 +50,8 @@ typedef enum
     GET,
     POST,
     PATCH,
-    DELETE
+    DELETE,
+    UPGRADE,
 } request_type;
 
 typedef struct
@@ -62,6 +79,7 @@ resp_status_code_t statusCodes[] = {
     {999, "xxxxxxxxxxxxxxxxxxxxx"}};
 
 resp_status_code_t *getResponseStatusCode(int);
+int Base64Encode(char *dest, const char *message);
 
 int main(int argc, char **argv)
 {
@@ -86,6 +104,31 @@ int main(int argc, char **argv)
 
 void servWS(void)
 {
+    static struct timeval timeout;
+    static int rv;
+    static fd_set set;
+    static char read_buffer[READ_BUFFER_LEN];                                       // read buffer
+    static const unsigned char text[] = {0x81, 0x05, 0x48, 0x65, 0x6c, 0x6c, 0x6f}; // hello
+    static int acceptLen;
+    int i;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 900;
+    for (int wsClient : wsClients)
+    {
+        // readのtimeout
+        FD_ZERO(&set);          /* clear the set */
+        FD_SET(wsClient, &set); /* add our file descriptor to the set */
+        rv = select(wsClient + 1, &set, NULL, NULL, &timeout);
+        if (rv <= 0)
+            continue;
+        acceptLen = read(wsClient, read_buffer, READ_BUFFER_LEN);
+        if (acceptLen == 0)
+            continue;
+        read_buffer[acceptLen] = 0;
+        printf("%s\n", read_buffer);
+        printBuffer(read_buffer, acceptLen);
+        close(wsClient);
+    }
 }
 
 void servHTML(int servSock)
@@ -117,22 +160,40 @@ void servHTML(int servSock)
     }
     std::cout << "connected from " << inet_ntoa(cliSockAddr.sin_addr) << "." << std::endl;
     acceptLen = read(cliSock, read_buffer, READ_BUFFER_LEN);
+    read_buffer[acceptLen] = 0;
+    std::cout << "read_buffer: " << read_buffer << std::endl;
     std::vector<std::string> requestHeaders = StrUtils::split(read_buffer, "\n");
     std::vector<std::string> requestLine = StrUtils::split(requestHeaders.at(0), " ");
-#ifdef __DEBUG
-    std::vector<std::string> requestParams;
-    std::copy_if(requestHeaders.begin() + 1, requestHeaders.end(), std::back_inserter(requestParams), [](const std::string &item)
+    std::map<std::string, std::string> requestHeader;
+    std::vector<std::string> requestHeaderLines;
+    std::copy_if(requestHeaders.begin() + 1, requestHeaders.end(), std::back_inserter(requestHeaderLines), [](const std::string &item)
                  { return StrUtils::trim(item).length() >= 1; });
-    for (std::string r_param : requestParams)
+    for (std::string r_param : requestHeaderLines)
     {
         std::vector<std::string> p = StrUtils::split(r_param, ":");
-
-        std::cout << "\"" << p.at(0) << "\" \"" << StrUtils::trim(p.at(1)) << "\"" << std::endl;
-    }
+        if (p.size() < 2)
+        {
+            continue;
+        }
+#ifdef __DEBUG
+        // std::cout << "\"" << p.at(0) << "\" \"" << StrUtils::trim(p.at(1)) << "\"" << std::endl;
 #endif
+        requestHeader[p.at(0)] = StrUtils::trim(p.at(1));
+    }
+
+    std::cout << "requestLine: " << requestHeaders.at(0) << std::endl;
     if (requestLine.at(0) == "GET")
     {
-        rtype = request_type::GET;
+        if (requestHeader.count("Upgrade") != 0 && requestHeader["Upgrade"] == "websocket")
+        {
+            rtype = request_type::UPGRADE;
+            std::cout << "Upgrade: " << requestHeader["Upgrade"] << std::endl;
+            std::cout << "Sec-WebSocket-Key: " << requestHeader["Sec-WebSocket-Key"] << std::endl;
+        }
+        else
+        {
+            rtype = request_type::GET;
+        }
     }
     else if (requestLine.at(0) == "POST")
     {
@@ -145,7 +206,8 @@ void servHTML(int servSock)
 
     std::vector<std::string> requestContent = StrUtils::split(requestLine.at(1), "?");
     std::string request_path = StrUtils::ltrim(requestContent.at(0), "/");
-    if(request_path.length() == 0) {
+    if (request_path.length() == 0)
+    {
         request_path = "index.html";
     }
     std::cout << "/" << request_path << std::endl;
@@ -176,9 +238,9 @@ void servHTML(int servSock)
     resp_status_code_t *respStatusCode = (rtype == request_type::UNKNOWN) ? getResponseStatusCode(405) : getResponseStatusCode(200);
     if (rtype == request_type::GET)
     {
-        std::cout << "GET" << std::endl;
+        // std::cout << "GET" << std::endl;
         path file_path = assets_dir / request_path;
-        std::cout << "searching: " << assets_dir << " " << assets_dir / request_path << std::endl;
+        // std::cout << "searching: " << assets_dir << " " << assets_dir / request_path << std::endl;
         if (exists(file_path) && is_regular_file(file_path))
         {
             if (file_path.extension() == ".jpg" || file_path.extension() == ".jpeg" || file_path.extension() == ".JPG")
@@ -215,13 +277,26 @@ void servHTML(int servSock)
             {
                 ifs.read(resp_buffer, READ_BUFFER_LEN);
                 write(cliSock, resp_buffer, ifs.gcount());
-            }while (ifs.gcount() > 0);
+            } while (ifs.gcount() > 0);
 
             ifs.close();
         }
 
         write(cliSock, "\r\n\r\n", 4);
         close(cliSock);
+    }
+    else if (rtype == request_type::UPGRADE)
+    {
+        respStatusCode = getResponseStatusCode(101);
+        std::string websocket_key = requestHeader["Sec-WebSocket-Key"] + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+
+        SHA1((const unsigned char *)websocket_key.c_str(), websocket_key.length(), websocket_sha1_buf);
+        Base64Encode((char *)websocket_b64_buf, (const char *)websocket_sha1_buf);
+        snprintf(resp_buffer, RESP_BUFFER_LEN, RESP, respStatusCode->number, respStatusCode->message);
+        write(cliSock, resp_buffer, strlen(resp_buffer));
+        snprintf(resp_buffer, RESP_BUFFER_LEN, "Upgrade: websocket\r\nConnection: upgrade\r\nSec-WebSocket-Accept: %s\n\n", websocket_b64_buf);
+        write(cliSock, resp_buffer, strlen(resp_buffer));
+        wsClients.push_back(cliSock);
     }
 }
 
@@ -235,4 +310,32 @@ resp_status_code_t *getResponseStatusCode(int code)
         }
     }
     return nullptr;
+}
+
+int Base64Encode(char *dest, const char *message)
+{ // Encodes a string to base64
+    BIO *bio, *b64;
+    FILE *stream;
+    int encodedSize = 4 * ceil((double)SHA_DIGEST_LENGTH / 3);
+
+    stream = fmemopen(dest, encodedSize + 1, "w");
+    b64 = BIO_new(BIO_f_base64());
+    bio = BIO_new_fp(stream, BIO_NOCLOSE);
+    bio = BIO_push(b64, bio);
+    BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL); // Ignore newlines - write everything in one line
+    BIO_write(bio, message, SHA_DIGEST_LENGTH);
+    BIO_flush(bio);
+    BIO_free_all(bio);
+    fclose(stream);
+    return (0); // success
+}
+
+void printBuffer(void *buffer, size_t length)
+{
+    uint8_t *ubuf = (uint8_t *)buffer;
+    for (size_t i = 0; i < length; ++i)
+    {
+        printf("0x%02X ", ubuf[i]);
+    }
+    printf("\n");
 }
